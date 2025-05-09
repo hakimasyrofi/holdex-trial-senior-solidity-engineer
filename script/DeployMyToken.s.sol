cd0967ddb7e2b267708417ccccef6da4a303f52a
hakimasyrofi <hakimasyrofi@gmail.com>
2025-05-09 14:54:03 +0800

    chore: enhance documentation for MyToken and DeployMyToken scripts

--- script/DeployMyToken.s.sol
+++ script/DeployMyToken.s.sol
@@ -4,7 +4,16 @@ pragma solidity ^0.8.20;
 import {Script, console} from "forge-std/Script.sol";
 import {MyToken} from "../src/MyToken.sol";
 
+/**
+ * @title MyToken Deployment Script
+ * @notice This script handles the deployment of the MyToken contract
+ * @dev Uses Foundry's Script utilities for deployment
+ */
 contract DeployMyToken is Script {
+    /**
+     * @notice Deploys a new instance of the MyToken contract
+     * @return MyToken The deployed token contract instance
+     */
     function deployMyToken() external returns (MyToken) {
         vm.startBroadcast();
         MyToken myToken = new MyToken();
